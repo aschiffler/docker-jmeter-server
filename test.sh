f2cf48dd66dafec2d17f8c7798068f8a6c478a28
Andreas Schiffler <andreas.schifflerthws.de>
2023-09-15 13:06:45 +0200

    addaptions

--- test.sh
+++ test.sh
@@ -1,16 +1,11 @@
 #!/bin/bash
-#
-# Test the JMeter Docker image using a trivial test plan.
 
-# Example for using User Defined Variables with JMeter
-# These will be substituted in JMX test script
-# See also: http://stackoverflow.com/questions/14317715/jmeter-changing-user-defined-variables-from-command-line
-export TARGET_HOST="www.map5.nl"
-export TARGET_PORT="80"
-export TARGET_PATH="/kaarten.html"
-export TARGET_KEYWORD="Kaartdiensten"
+export TARGET_HOST="broker.hivemq.com"
+export PLAN="sub_pub.jmx"
 
-T_DIR=tests/trivial
+
+#----------------------------------
+T_DIR=tests/mqtt
 
 # Reporting dir: start fresh
 R_DIR=${T_DIR}/report
@@ -19,10 +14,9 @@ mkdir -p ${R_DIR}
 
 /bin/rm -f ${T_DIR}/test-plan.jtl ${T_DIR}/jmeter.log  > /dev/null 2>&1
 
-./run.sh -Dlog_level.jmeter=DEBUG \
-	-JTARGET_HOST=${TARGET_HOST} -JTARGET_PORT=${TARGET_PORT} \
-	-JTARGET_PATH=${TARGET_PATH} -JTARGET_KEYWORD=${TARGET_KEYWORD} \
-	-n -t ${T_DIR}/test-plan.jmx -l ${T_DIR}/test-plan.jtl -j ${T_DIR}/jmeter.log \
+jmeter -Dlog_level.jmeter=DEBUG \
+	-JBROKER=${TARGET_HOST}  \
+	-n -t ${T_DIR}/${PLAN} -l ${T_DIR}/test-plan.jtl -j ${T_DIR}/jmeter.log \
 	-e -o ${R_DIR}
 
 echo "==== jmeter.log ===="
