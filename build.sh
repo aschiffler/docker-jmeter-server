a478739bb45fb9c3eb438ade7347cab9e8f8bfb4
Andreas Schiffler <schiffler@roschi.eu>
2023-09-17 16:52:02 +0000

    add roschi docker repo

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 JMETER_VERSION=${JMETER_VERSION:-"5.6"}
 IMAGE_TIMEZONE=${IMAGE_TIMEZONE:-"Europe/Amsterdam"}
-REPO="yourrepo"
+REPO="roschi"
 
 # Example build line
 docker build  --build-arg JMETER_VERSION=${JMETER_VERSION} --build-arg TZ=${IMAGE_TIMEZONE} -t "${REPO}/jmeter-mqtt:${JMETER_VERSION}" .
--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@
 
 NAME="jmeter"
 JMETER_VERSION=${JMETER_VERSION:-"5.6"}
-IMAGE="yourrepo/jmeter-mqtt:${JMETER_VERSION}"
+IMAGE="roschi/jmeter-mqtt:${JMETER_VERSION}"
 
 # Finally run
 docker run --rm --name ${NAME} -i -v ${PWD}:${PWD} -w ${PWD} ${IMAGE} $@
